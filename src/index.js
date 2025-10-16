e47f7bee2159753bc120495306b55eb3a5339d8a
KouamefredDev <kouamefred.dev@gmail.com>
2025-10-16 12:39:23 +0000

    Correction du fichier index.js dans src

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/fred-folio">
       <App />
     </BrowserRouter>
   </StrictMode>
